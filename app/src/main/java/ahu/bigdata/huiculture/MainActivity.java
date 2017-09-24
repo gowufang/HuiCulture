21dab34dd742ccff75b262c552e7bfc9c4988f01
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-09-24 16:00:53 +0800

    logo

--- app/src/main/java/ahu/bigdata/huiculture/MainActivity.java
+++ app/src/main/java/ahu/bigdata/huiculture/MainActivity.java
@@ -35,9 +35,6 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        //隐藏TitleBar
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_main);
         initData();
         initView();
