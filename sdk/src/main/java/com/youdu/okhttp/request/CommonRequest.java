fa47006228c94d7d532357eea60dde989fd1e916
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-09-30 19:32:32 +0800

    packaged the compnents of okhttp
    next tip:load image

--- sdk/src/main/java/com/youdu/okhttp/request/CommonRequest.java
+++ sdk/src/main/java/com/youdu/okhttp/request/CommonRequest.java
@@ -23,8 +23,42 @@ public class CommonRequest {
      * @return 返回一个创建好的Request对象
      */
     public static Request createPostRequest(String url, RequestParams params) {
-        return createPostRequest(url, params, null);
+
+        FormBody.Builder mFormBodyBuild = new FormBody.Builder();
+        if (params != null) {
+            /**
+             *  for (循环变量类型 循环变量名称 : 要被遍历的对象) 循环体
+             */
+            for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+
+                //将请求参数遍历添加到请求构建类中
+                mFormBodyBuild.add(entry.getKey(), entry.getValue());
+            }
+        }
+        //通过请求构建类的build方法获得真正的请求体对象
+        FormBody mFormBody = mFormBodyBuild.build();
+        return new Request.Builder().url(url).post(mFormBody).build();
     }
+
+    /**
+     * @param url
+     * @param params
+     * @return 返回一个创建好的Get对象,将Url和params生成字符串再生成Get类型Request的get对象
+     */
+    public static Request createGetRequest(String url,RequestParams params) {
+
+        StringBuilder urlBuilder = new StringBuilder(url).append("?");
+        if (params!=null) {
+
+            for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+                urlBuilder.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+            }
+
+        }
+        return new Request.Builder().url(urlBuilder.substring(0, urlBuilder.length() - 1)).get().build();
+
+    }
+
 
     /**可以带请求头的Post请求
      * @param url
@@ -35,7 +69,12 @@ public class CommonRequest {
     public static Request createPostRequest(String url, RequestParams params, RequestParams headers) {
         FormBody.Builder mFormBodyBuild = new FormBody.Builder();
         if (params != null) {
+            /**
+             *  for (循环变量类型 循环变量名称 : 要被遍历的对象) 循环体
+             */
             for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+
+                //将请求参数遍历添加到请求构建类中
                 mFormBodyBuild.add(entry.getKey(), entry.getValue());
             }
         }
@@ -55,17 +94,6 @@ public class CommonRequest {
         return request;
     }
 
-    /**
-     * ressemble the params to the url
-     *
-     * @param url
-     * @param params
-     * @return
-     */
-    public static Request createGetRequest(String url, RequestParams params) {
-
-        return createGetRequest(url, params, null);
-    }
 
     /**
      * 可以带请求头的Get请求
