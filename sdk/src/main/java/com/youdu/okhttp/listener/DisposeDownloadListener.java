3a40bd4e13fda9da72c68a4be90f80faaee6d31d
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-09-30 17:06:49 +0800

    删除了sdk

--- sdk/src/main/java/com/youdu/okhttp/listener/DisposeDownloadListener.java
+++
@@ -1,9 +0,0 @@
-package com.youdu.okhttp.listener;
-
-/**
- * @author vision
- * @function 监听下载进度
- */
-public interface DisposeDownloadListener extends DisposeDataListener {
-	public void onProgress(int progrss);
-}
