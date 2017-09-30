8be0ba8d2fbf6fb47bf717e21c912e1220e1437b
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-09-30 17:18:12 +0800

    加入了sdk，并修复了Bug
    下一步完成主页

---
+++ sdk/src/main/java/com/youdu/BaseOkhttpTest.java
@@ -0,0 +1,15 @@
+package com.youdu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by YCH on 2017/9/30.
+ * Function:测试Okhttp
+ */
+public class BaseOkhttpTest extends AppCompatActivity {
+
+        
+
+
+
+}
