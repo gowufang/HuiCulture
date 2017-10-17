f5904c0280f03cc73446fb687a793be5ff5b367b
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-10-17 16:49:05 +0800

    ……

--- app/src/main/java/ahu/bigdata/huiculture/module/recommand/RecommandHeadValue.java
+++ app/src/main/java/ahu/bigdata/huiculture/module/recommand/RecommandHeadValue.java
@@ -9,6 +9,6 @@ import java.util.ArrayList;
 public class RecommandHeadValue {
 
     public ArrayList<String> ads;
-    public ArrayList<String> middle;
-    public ArrayList<RecommandFooterValue> footer;
-}
+public ArrayList<String> middle;
+public ArrayList<RecommandFooterValue> footer;
+        }
