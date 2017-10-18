c831e8107fb8e431dbffb9028ee9d08d04ef0abd
ych2017 <30399716+ych2017@users.noreply.github.com>
2017-10-18 16:30:24 +0800

    Revert "尝试了一下vassonic，失败了"
    This reverts commit 462cfa4dfdc66a860d97f1055bc054903f5cd112.

--- app/src/main/java/ahu/bigdata/huiculture/activity/WebViewActivity.java
+++ app/src/main/java/ahu/bigdata/huiculture/activity/WebViewActivity.java
@@ -1,44 +1,18 @@
 package ahu.bigdata.huiculture.activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-
-import com.tencent.sonic.sdk.SonicConfig;
-import com.tencent.sonic.sdk.SonicConstants;
-import com.tencent.sonic.sdk.SonicEngine;
-import com.tencent.sonic.sdk.SonicSession;
-import com.tencent.sonic.sdk.SonicSessionConfig;
-import com.tencent.sonic.sdk.SonicSessionConnection;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.ref.WeakReference;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import ahu.bigdata.huiculture.R;
 import ahu.bigdata.huiculture.activity.base.BaseActivity;
 import ahu.bigdata.huiculture.utils.L;
-import ahu.bigdata.huiculture.web.SonicJavaScriptInterface;
-import ahu.bigdata.huiculture.web.SonicRuntimeImpl;
-import ahu.bigdata.huiculture.web.SonicSessionClientImpl;
 
 /**
  * Created by YCH on 2017/9/27.
@@ -47,169 +21,21 @@ import ahu.bigdata.huiculture.web.SonicSessionClientImpl;
 
 public class WebViewActivity extends BaseActivity {
 
-    public final static String PARAM_URL = "param_url";
-
-    public final static String PARAM_MODE = "param_mode";
-    private SonicSession sonicSession;
-
-//
-//      //进度条
-//    private ProgressBar mProgrecessBar;
-//    //网页
-//    private WebView mWebView;
-
+    //进度条
+    private ProgressBar mProgrecessBar;
+    //网页
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initView();
-    }
-
-
-    private void initView() {
-
+        setContentView(R.layout.activity_webview);
         /**
          *显示箭头返回
          */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Intent intent = getIntent();
-        final String Url = intent.getStringExtra("url");
-        int mode = intent.getIntExtra(PARAM_MODE, -1);
-        if (TextUtils.isEmpty(Url) || -1 == mode) {
-            finish();
-            return;
-        }
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-
-        // step 1: Initialize sonic engine if necessary, or maybe u can do this when application created
-        if (!SonicEngine.isGetInstanceAllowed()) {
-            SonicEngine.createInstance(new SonicRuntimeImpl(getApplication()), new SonicConfig.Builder().build());
-        }
-        SonicSessionClientImpl sonicSessionClient = null;
-
-        // step 2: Create SonicSession
-        sonicSession = SonicEngine.getInstance().createSession(Url,  new SonicSessionConfig.Builder().build());
-        if (null != sonicSession) {
-            sonicSession.bindClient(sonicSessionClient = new SonicSessionClientImpl());
-        } else {
-            // this only happen when a same sonic session is already running,
-            // u can comment following codes to feedback as a default mode.
-            throw new UnknownError("create session fail!");
-        }
-
-        // step 3: BindWebView for sessionClient and bindClient for SonicSession
-        // in the real world, the init flow may cost a long time as startup
-        // runtime、init configs....
-        setContentView(R.layout.activity_webview);
-        WebView webView = (WebView) findViewById(R.id.mWebView);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                if (sonicSession != null) {
-                    sonicSession.getSessionClient().pageFinish(url);
-                }
-            }
-
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    return shouldInterceptRequest(view, request.getUrl().toString());
-                }
-                return shouldInterceptRequest(view, request.getUrl().toString());
-            }
-
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-                if (sonicSession != null) {
-                    //step 6: Call sessionClient.requestResource when host allow the application
-                    // to return the local data .
-                    return (WebResourceResponse) sonicSession.getSessionClient().requestResource(url);
-                }
-                return null;
-            }
-        });
-
-        WebSettings webSettings = webView.getSettings();
-
-        // step 4: bind javascript
-        // note:if api level lower than 17(android 4.2), addJavascriptInterface has security
-        // issue, please use x5 or see https://developer.android.com/reference/android/webkit/
-        // WebView.html#addJavascriptInterface(java.lang.Object, java.lang.String)
-        webSettings.setJavaScriptEnabled(true);
-        webView.removeJavascriptInterface("searchBoxJavaBridge_");
-        intent.putExtra(SonicJavaScriptInterface.PARAM_LOAD_URL_TIME, System.currentTimeMillis());
-        webView.addJavascriptInterface(new SonicJavaScriptInterface(sonicSessionClient, intent), "sonic");
-
-        // init webview settings
-        webSettings.setAllowContentAccess(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setSavePassword(false);
-        webSettings.setSaveFormData(false);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLoadWithOverviewMode(true);
-
-
-        // step 5: webview is ready now, just tell session client to bind
-        if (sonicSessionClient != null) {
-            sonicSessionClient.bindWebView(webView);
-            sonicSessionClient.clientReady();
-        } else { // default mode
-            webView.loadUrl(Url);
-        }
-
-
-
-
-
-
-//        mProgrecessBar = (ProgressBar) findViewById(R.id.mProgressBar);
-//        mWebView = (WebView) findViewById(R.id.mWebView);
-//
-//        Intent intent = getIntent();
-//
-//        final String Url = intent.getStringExtra("url");
-//        //设置标题
-//        getSupportActionBar().setTitle(intent.getStringExtra("title"));
-//        L.d("---------------Url get:---------------"+Url);
-//
-//        //进行加载网页的逻辑
-//        mWebView.getSettings().setJavaScriptEnabled(true);
-//        //支持缩放
-//        mWebView.getSettings().setSupportZoom(true);
-//        mWebView.getSettings().setBuiltInZoomControls(true);
-//        //接口回调
-//        mWebView.setWebChromeClient(new WebViewClient());
-//        //加载网页
-//        mWebView.loadUrl(Url);
-//        //本地显示
-//        mWebView.setWebViewClient(new android.webkit.WebViewClient(){
-//            /**
-//             *一直加载会跳转浏览器，ture表示我接受这个事件
-//             */
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-//
-//                view.loadUrl(Url);
-//                return true;
-//            }
-//        });
-//    }
-//    public class WebViewClient extends WebChromeClient{
-//
-//        //进度变化的监听
-//        @Override
-//        public void onProgressChanged(WebView view, int newProgress) {
-//
-//            if (newProgress==100) {
-//                mProgrecessBar.setVisibility(View.GONE);
-//            }
-//            super.onProgressChanged(view, newProgress);
-//        }
+        initView();
     }
+
     //菜单栏
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -222,73 +48,50 @@ public class WebViewActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
+    private void initView() {
 
-    @Override
-    protected void onDestroy() {
-        if (null != sonicSession) {
-            sonicSession.destroy();
-            sonicSession = null;
-        }
-        super.onDestroy();
-    }
+        mProgrecessBar = (ProgressBar) findViewById(R.id.mProgressBar);
+        mWebView = (WebView) findViewById(R.id.mWebView);
 
-    private static class OfflinePkgSessionConnection extends SonicSessionConnection {
+        Intent intent = getIntent();
 
-        private final WeakReference<Context> context;
+        final String Url = intent.getStringExtra("url");
+        //设置标题
+        getSupportActionBar().setTitle(intent.getStringExtra("title"));
+        L.d("---------------Url get:---------------"+Url);
 
-        public OfflinePkgSessionConnection(Context context, SonicSession session, Intent intent) {
-            super(session, intent);
-            this.context = new WeakReference<Context>(context);
-        }
+        //进行加载网页的逻辑
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //支持缩放
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        //接口回调
+        mWebView.setWebChromeClient(new WebViewClient());
+        //加载网页
+        mWebView.loadUrl(Url);
+        //本地显示
+        mWebView.setWebViewClient(new android.webkit.WebViewClient(){
+            /**
+             *一直加载会跳转浏览器，ture表示我接受这个事件
+             */
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
 
-        @Override
-        protected int internalConnect() {
-            Context ctx = context.get();
-            if (null != ctx) {
-                try {
-                    InputStream offlineHtmlInputStream = ctx.getAssets().open("sonic-demo-index.html");
-                    responseStream = new BufferedInputStream(offlineHtmlInputStream);
-                    return SonicConstants.ERROR_CODE_SUCCESS;
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
+                view.loadUrl(Url);
+                return true;
             }
-            return SonicConstants.ERROR_CODE_UNKNOWN;
-        }
+        });
+    }
+    public class WebViewClient extends WebChromeClient{
 
+        //进度变化的监听
         @Override
-        protected BufferedInputStream internalGetResponseStream() {
-            return responseStream;
-        }
+        public void onProgressChanged(WebView view, int newProgress) {
 
-        @Override
-        public void disconnect() {
-            if (null != responseStream) {
-                try {
-                    responseStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (newProgress==100) {
+                mProgrecessBar.setVisibility(View.GONE);
             }
-        }
-
-        @Override
-        public int getResponseCode() {
-            return 200;
-        }
-
-        @Override
-        public Map<String, List<String>> getResponseHeaderFields() {
-            return new HashMap<>(0);
-        }
-
-        @Override
-        public String getResponseHeaderField(String key) {
-            return "";
+            super.onProgressChanged(view, newProgress);
         }
     }
 }
--- app/src/main/java/ahu/bigdata/huiculture/fragment/home/CloudFragment.java
+++ app/src/main/java/ahu/bigdata/huiculture/fragment/home/CloudFragment.java
@@ -1,19 +1,12 @@
 package ahu.bigdata.huiculture.fragment.home;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-
-import com.tencent.sonic.sdk.SonicConfig;
-import com.tencent.sonic.sdk.SonicEngine;
 
 import java.util.ArrayList;
 
@@ -23,10 +16,6 @@ import ahu.bigdata.huiculture.adapter.GridViewAdapter;
 import ahu.bigdata.huiculture.constant.Constant;
 import ahu.bigdata.huiculture.fragment.BaseFragment;
 import ahu.bigdata.huiculture.module.cloud.GridItem;
-import ahu.bigdata.huiculture.web.SonicJavaScriptInterface;
-import ahu.bigdata.huiculture.web.SonicRuntimeImpl;
-
-import static android.R.attr.mode;
 
 /**
  * Created by ych10 on 2017/9/21.
@@ -44,13 +33,6 @@ public class CloudFragment extends BaseFragment implements AdapterView.OnItemCli
      */
     private GridViewAdapter mGridViewAdapter = null;
     private ArrayList<GridItem> mGridData = null;
-    public static final int MODE_DEFAULT = 0;
-
-    public static final int MODE_SONIC = 1;
-
-    public static final int MODE_SONIC_WITH_OFFLINE_CACHE = 2;
-
-    private static final int PERMISSION_REQUEST_CODE_STORAGE = 1;
 
 
     @Override
@@ -78,13 +60,6 @@ public class CloudFragment extends BaseFragment implements AdapterView.OnItemCli
             mGridData.add(item);
         }
 
-        if (hasPermission()) {
-            init();
-        } else {
-            requestPermission();
-        }
-
-
     }
 
     private void initView() {
@@ -106,44 +81,7 @@ public class CloudFragment extends BaseFragment implements AdapterView.OnItemCli
     private void conveyURL(int position) {
         Intent intent = new Intent(mContext, WebViewActivity.class);
         intent.putExtra("url",Constant.LogoWebUrl[position]);
-        intent.putExtra(WebViewActivity.PARAM_MODE, MODE_SONIC_WITH_OFFLINE_CACHE);
         intent.putExtra("title", Constant.LogoTextUrl[position]);
-        intent.putExtra(SonicJavaScriptInterface.PARAM_CLICK_TIME, System.currentTimeMillis());
-        startActivityForResult(intent,-1);
+        startActivity(intent);
     }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (PERMISSION_REQUEST_CODE_STORAGE == requestCode) {
-            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                requestPermission();
-            } else {
-                init();
-            }
-            return;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void init() {
-        // init sonic engine
-        if (!SonicEngine.isGetInstanceAllowed()) {
-            SonicEngine.createInstance(new SonicRuntimeImpl(mContext), new SonicConfig.Builder().build());
-        }
-    }
-
-    private boolean hasPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return checkSelfP ermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-        }
-        return true;
-    }
-
-    private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE_STORAGE);
-        }
-    }
-
 }
